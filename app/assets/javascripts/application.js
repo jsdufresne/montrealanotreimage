9ed8571e149a190bf27a92a06a163f1426cbad30
Jean-Sebastien <jsdufresne@iciela.org>
2018-09-03 16:48:54 -0400

    Refonte de la page d'accueil

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,3 +27,10 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
